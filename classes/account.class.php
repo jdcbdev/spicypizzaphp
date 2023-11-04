c74a3191d7bdca65b96e71ab0cece0b1df19108f
Jaydee Ballaho <jaydee.ballaho@wmsu.edu.ph>
2023-11-04 18:43:45 +0800

    Create account.class.php

---
+++ classes/account.class.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'database.php';
+
+class Account{
+
+    public $id;
+    public $email;
+    public $password;
+
+    protected $db;
+
+    function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    function sign_in_staff(){
+        $sql = "SELECT * FROM staff WHERE BINARY email = :email LIMIT 1;";
+        $query = $this->db->connect()->prepare($sql);
+        $query->bindParam(':email', $this->email);
+    
+        if ($query->execute()) {
+            $accountData = $query->fetch(PDO::FETCH_ASSOC);
+    
+            if ($accountData && password_verify($this->password, $accountData['password'])) {
+                $this->id = $accountData['id'];
+                return true;
+            }
+        }
+    
+        return false;
+    }    
+
+}
+
+?>
